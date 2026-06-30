b7c3588c45513a7afd3e6e16e3e73d16a5d66cdd
Nithin K R <nithinkr5209@gmail.com>
2026-06-30 06:49:08 +0530

    Add files via upload

---
+++ file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "file.h"
+
+void saveContactsToFile(AddressBook *addressBook) 
+{
+  FILE *fp;
+    fp = fopen("contact.csv","w");
+
+    if(fp==NULL)
+    {
+        fprintf(stderr,"file not found");
+        return;
+
+    }
+
+    for(int i =0 ; i<addressBook->contactCount;i++)
+    {
+        fprintf(fp , "%s,%s,%s\n",addressBook->contacts[i].name,addressBook->contacts[i].phone,addressBook->contacts[i].email);
+    }
+    fclose(fp);
+}
+
+void loadContactsFromFile(AddressBook *addressBook)
+{
+    FILE *fp;
+    fp = fopen("contact.csv","r");
+
+    if(fp==NULL)
+    {
+        fprintf(stderr,"file not found");
+        return;
+
+    }
+
+    while(fscanf(fp , "%[^,],%[^,],%[^\n]\n",addressBook->contacts[addressBook->contactCount].name,addressBook->contacts[addressBook->contactCount].phone,addressBook->contacts[addressBook->contactCount].email)==3)
+    {
+        addressBook->contactCount++;
+    }
+    fclose(fp);
+
+    
+}
---
+++ makefile
@@ -0,0 +1,12 @@
+Addressbook : main.o contact.o file.o populate.o
+	gcc -o Addressbook main.o contact.o file.o populate.o
+main.o:main.c
+	gcc -c main.c
+contact.o:contact.c
+	gcc -c contact.c
+file.o:file.c
+	gcc -c file.c
+populate.o:populate.c
+	gcc -c populate.c
+clean :
+	rm *.Addressbook*.o
